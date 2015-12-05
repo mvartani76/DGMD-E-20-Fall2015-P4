60dc87b916bf599c63f1fe00173700363bf66b5d
mvartani76 <mike.vartanian@gmail.com>
2015-12-05 02:23:09 -0500

    Replaced the content of the services page to be web site oriented instead of financial services... It is now a three column layout.. Added some Social Add buttons to the footer... Still need to format the footer some more...

--- index.php
+++ index.php
@@ -78,8 +78,14 @@
       </div>
     </main>
 
-    <div data-role="footer" data-position="fixed">
-      <p>&copy; 2015 Mike Vartanian</p>
+    <div data-role="footer" data-position="fixed" style="text-align:center;">
+      &copy; 2015 Mike Vartanian
+      <div data-role="controlgroup" data-type="horizontal">
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On Facebook</a>
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On Twitter</a>
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On LinkedIn</a>
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On Instagram</a>
+      </div>
     </div><!-- /footer -->
   </section><!-- End of Home Page -->
   
@@ -95,78 +101,61 @@
         </ul>
       </nav>
     </div>
+
     <main data-role="main" class="ui-content">
-
-    <div class="servicestitle">My Services</div>
-    <div class="ui-grid-a">
+    <h1 class="servicestitle">My Services</h1>
+    <p>I have been providing very assignment relevant website designs for a couple years now and can utillize this knowledge
+    for your small or medium sized business.</p>
+    
+    <p>As a small/medium sized business owner, you intimately understand the balance of money in and money out. I understand
+      this too. And since I only have a few web design classes under my belt, I offer this lack of skill and experience at a
+      very affordable price. I will work hard finding creative ways to build affordable websites that kick-ass!
+      A great website doesn't have to cost thousands, but it does have to be awesome!</p>
+    <div class="ui-grid-b">
       <div class="ui-block-a">
-        <h2 class="servicesh2lighterblue">Retail Merchant Account:</h2>
-          <img class="creditcardspic" src="./images/credit-cards.jpg">
-          <p>I will support all the major payment types for you or your business needs.  I just want to be paid back... :)</p>
-          <p>I have competitive rates for all four of the major credit and debit cards. I also support Diners Club®, Carte Blanche®, JCB®, stored value cards,
-            corporate cards, EBT cards, WEX, Voyager, Fleet One and Fuelman. If I currently do not support your card of choice, I will make sure to quickly adopt your
-            card as I am mostly concerned about getting paid back quickly...</p>
-          <p>You get quick and reliable settlement to your financial institution so I get paid back quicker.
-          Again, to make sure that I get paid back on time, I am set up to take care of all your merchant needs and requirements.</p>
-          <p class="clear">&nbsp;</p>
-      
-        <h2 class="servicesh2darkerblue">Gas Stations and Convenience Store Solutions:</h2>
-          <img src="./images/gas-station.jpg" class="gasstationpic">
-          <p>Since I have put a lot of time in to methods for paying me back, I figured I should utilize some of this knowledge into other areas and generate potential revenue from it.
-            One of these areas is the gas station and convenience store payment market.</p>
-          <p>My options in this area include stand-alone terminals, ECRs (Electronic Cash Registers), and pay-at-the-pump applications in either petroleum or c-store environments.
-          I am currently working on integrating some of my solutions with GasTV units and other pump based infotainment devices.</p>
-          <p>Again, I support all major credit and debit cards, private label and Fleet cards, including Wright Express, Voyager, Fuelman, Visa and MasterCard Fleet, and Prepaid Cards.</p>
-          
-          <p class="clear">&nbsp;</p>
-
-        <h2 class="servicesh2lighterblue">Restaurant Merchant Account:</h2>
-        <img src="./images/POS_Restaurant_touchgirl.jpg" class="restmerchacctpic">
-          <p>I can offer a variety of solutions to meet your processing needs, including point-of-sale (POS) interfaces stand-alone terminals and PC products.
-          I integrate with major restaurant management systems including authorization systems to report reconciliations.</p>
-          <p>I have a variety of restaurant applications for both Android and iOS devices so your servers and managers can use mobile devices such as smartphones and tablets to take orders, observe inventories and process payments.</p>
-        <p class="clear">&nbsp;</p>
-
-        <h2 class="servicesh2lighterblue">Pin-based Debit:</h2>
-          <img src="./images/pinbaseddebit.jpg" class="pinbaseddebitpic">
-          <ul>
-            <li>PIN prompting for debit cards allows the transaction to be routed to the more economical debit networks.</li>
-            <li>Dynamic routing of PIN-based debit cards to the least expensive debit networks increases the retailer’s margins.</li>
-            <li>Provides a secure form of payment</li>
-            <li>Allows you to accept PIN-based debit cards for BILL PAYMENT</li>
-          </ul>
+       <h2 class="ui-bar">Web Design</h2>
+       <p>A website is the core of any business. It is the first step to building a successful marketing strategy.
+        Think about it, when we hand out a business card, we are telling the recipient to visit our site. When we run
+        Google Ad campaigns, we are driving traffic to our website. When we meet someone, and the conversation turns into
+        what we do for a living, that person will inevitably visit our site. Everything we do drives traffic to our web site,
+        and unfortunately, most small business sites out there today are not good. The fact is, business owners and
+        entrepreneurs spend a lot of time and money driving traffic to websites that don't convert visitors into buyers.
+        Why? Because history has told us that if we want a webpage that is going to generate more clients and customers,
+        we have to pay a lot of money. Right? Wrong!</p>
       </div> <!-- End ui-block-a -->
       <div class="ui-block-b">
-        <h2 class="servicesh2darkerblue">Wireless Solutions:</h2>
-        <img src="./images/mp-iphone.png" class="mobilepayphonepic">
-        
-        <p>Mobility to your checkout process can improve customer service, expand your sales, and gets me paid back quicker.  Wireless transactions can be processed securely and swiftly, allowing your customer to pay on the spot.</p>
-        <p>Wireless point of sale terminal allows you to accept card types typically not accepted when conducting transactions without a dedicated phone line. Wireless payments requires only network connectivity for transaction processing.
-          If you keep your payment device in a fixed location and want to eradicate tangled telephone cords, or if you travel with your payment device and need the ability to process a transaction from a variety of locations, wireless solution is right for your business.</p>
-        <p>With my NEW Mobile Pay you can save money by downloading my app found in the Apple Store or Google Marketplace so you can utilize your smart phone or wireless device to run transactions.</P>
-
-        <img src="./images/mobile-payment-logos.png" class="mobilepaylogospic">
-          <p>I support the following wireless payment options: Apple Pay, Square, Intuit Go, Venmo, PayPal,
-            Google Wallet, SnapCash, Popmoney, <span id="lesspaymentoptions"> Dwolla, TabbedOut, Zong, Stripe,
-            mopay, obopay, PayOne, cellum, and mPass <span id="justlesspaymentoptions">(less).</span></span><span id="morepaymentoptions">and <span id="justmorepaymentoptions">more.</span></span></p>
-          <p>If you do not see your preferred wireless payment option, please contact me
-          directly and I will see what I can do to get it up and running as quickly as possible.</p>
-        <p class="clear">&nbsp;</p>
-
-        <h2 class="servicesh2darkerblue">eCommerce Solutions:</h2>
-          <img src="./images/keyboard_pay.png" class="pinbaseddebitpic">
-          <p>I have created several solutions that together have all the options to work with any merchant’s business online solution.  Variations within my several offerings are described below.</p>
-          <p><strong>Application Programming Interface (API) -</strong> An API allows you to connect the gateway directly with your web application for a seamless customer experience. It also enables a range of processing scenarios for flexibility in implementing custom business logic, helping you gain platform independence; offers ease of integration and transaction security.</p>
-
-          <p><strong>Payment Pages (Hosted Checkout) –</strong> Your payment processing is completely hosted by First Data and reduces PCI Scope. It rapidly integrates payment processing into your website, and optimizes for mobile devices so merchants can reach “on the go” consumers.</p>
-
-          <p><strong>Real-time Payment Manager (RPM) –</strong> The Virtual Terminal works as base access point for the gateway solution. Its back-office web product allows you to easily look up transaction activity, service your consumers and reconcile your online store. Functionalities include detailed reporting, transaction dashboard, online virtual point-of-sale terminal, transaction history search, recurring payment plans, user administration and more.</P>
+        <h2 class="ui-bar">Web Hosting</h2>
+        <p>Today, it's no secret that building a strong internet presence can do more for your business than most forms of
+          offline marketing. From websites to email marketing, each aspect of your online presence plays a very important
+          role in finding new clients and engaging with customers. The core of your business online is your website. While
+          smart business owners invest time and money into a great website, most opt for the most inexpensive website hosting.
+          Why? Cheap, unreliable website hosting can lead to loss of revenue, negative effects on SEO Ranking, security
+          issues, and frustration. Furthermore, most website hosting companies don't support or troubleshoot issues, other
+          than those originating from their servers. What does this mean? This means that if your website isn't functioning
+          quite right, it's up to you to find a developer, or fix it yourself. So what's the solution?</p>
       </div> <!-- End block-b -->
-    </div> <!-- End grid-a -->
+      <div class="ui-block-c">
+        <h2 class="ui-bar">Online/Mobile Marketing Strategy</h2>
+        <p>The landscape and tactics used for online marketing are ever changing. What might work today, doesn't always work
+          tomorrow. Successful businesses are constantly evolving their marketing strategy to stay in-tune with what is
+          working, and spending less on what's not. From Email Marketing to pay-per-click ads, there are a number of ways to
+          market your business online. Some of these methods cost next to nothing, while others can cost a fortune. The
+          problem is, a lot of business owners don't know what will work, or what is currently working to drive traffic to
+          their website. This means a lot of wasted money on ineffective online strategies. We work with our clients to build
+          highly-effective online marketing strategies. No matter if it's a doctor's office or  a restaurant, each strategy
+          is a customized solution for best results. We'll show you exactly what to do to make the internet work for you!</p>
+      </div> <!-- End block-c -->
+    </div> <!-- End grid-b -->
 
     </main>
-    <div data-role="footer" data-position="fixed">
-      <p>&copy; 2015 Mike Vartanian</p>
+    <div data-role="footer" data-position="fixed" style="text-align:center;">
+      &copy; 2015 Mike Vartanian
+      <div data-role="controlgroup" data-type="horizontal">
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On Facebook</a>
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On Twitter</a>
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On LinkedIn</a>
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On Instagram</a>
+      </div>
     </div><!-- /footer -->
   </section><!-- End of Services Page -->
 
@@ -196,8 +185,14 @@
           </form>
       </div>
     </main>
-    <div data-role="footer" data-position="fixed">
-      <p>&copy; 2015 Mike Vartanian</p>
+    <div data-role="footer" data-position="fixed" style="text-align:center;">
+      &copy; 2015 Mike Vartanian
+      <div data-role="controlgroup" data-type="horizontal">
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On Facebook</a>
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On Twitter</a>
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On LinkedIn</a>
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On Instagram</a>
+      </div>
     </div><!-- /footer -->
   </section><!-- End of Login Page -->
   
@@ -246,8 +241,14 @@
         </form>
       </div>
     </main>
-    <div data-role="footer" data-position="fixed">
-      <p>&copy; 2015 Mike Vartanian</p>
+    <div data-role="footer" data-position="fixed" style="text-align:center;">
+      &copy; 2015 Mike Vartanian
+      <div data-role="controlgroup" data-type="horizontal">
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On Facebook</a>
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On Twitter</a>
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On LinkedIn</a>
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On Instagram</a>
+      </div>
     </div><!-- /footer -->
   </section><!-- End of Register Page -->
 
@@ -314,8 +315,14 @@
       </div>
     </main>
 
-    <div data-role="footer" data-position="fixed">
-      <p>&copy; 2015 Mike Vartanian</p>
+    <div data-role="footer" data-position="fixed" style="text-align:center;">
+      &copy; 2015 Mike Vartanian
+      <div data-role="controlgroup" data-type="horizontal">
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On Facebook</a>
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On Twitter</a>
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On LinkedIn</a>
+        <a href="#" class="ui-btn ui-corner-all ui-shadow ui-icon-plus ui-btn-icon-left">Add Me On Instagram</a>
+      </div>
     </div><!-- /footer -->
   </section><!-- End of Contact Page -->
 
